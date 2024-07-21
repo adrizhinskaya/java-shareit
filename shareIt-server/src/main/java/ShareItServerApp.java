cc9848a72e96084c4aafe039df49399d0113ee33
Alina <adrizhinskaya@bk.ru>
2024-07-21 16:51:12 +0300

    fix: checkstyle fix

--- shareIt-server/src/main/java/ShareItServerApp.java
+++ shareIt-server/src/main/java/ShareItServerApp.java
@@ -2,10 +2,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServerApp
-{
-    public static void main( String[] args )
-    {
+public class ShareItServerApp {
+    public static void main(String[] args) {
         SpringApplication.run(ShareItServerApp.class, args);
     }
 }
